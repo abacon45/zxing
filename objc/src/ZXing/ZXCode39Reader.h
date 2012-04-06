466e848294fbcae0dc0fdba244bfd84b46b11b5c
Patrick Hartling <patrick.hartling@gmail.com>
2012-04-06 09:39:38 -0500

    Add Code 39 and UPC-E reader wrappers.

---
+++ objc/src/ZXing/ZXCode39Reader.h
@@ -0,0 +1,13 @@
+//
+//  ZXCode39Reader.h
+//  ios
+//
+//  Created by Patrick Hartling on 4/4/12.
+//  Copyright (c) 2012 T8 Webware. All rights reserved.
+//
+
+#import "ZXReader.h"
+
+@interface ZXCode39Reader : ZXReader
+
+@end
